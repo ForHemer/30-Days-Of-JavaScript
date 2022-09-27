f509daeafe9de0152eb207e3451068a9cba74715
ForHemer <foreste.hemerson@gmail.com>
2022-09-27 10:59:01 -0400

    Exercices level 3 complete

---
+++ 00_Solutions/day-02/level3.js
@@ -0,0 +1,12 @@
+/* 1.- 'Love is the best thing in this world. Some found their love and some are still looking for their love'.
+ Count the number of word 'love' in this sentence. */ 
+ console.log('Love is the best thing in this world. Some found their love and some\
+  are still looking for their love'.match(/love/gi));
+
+  /* 2.- Use match() to count the number of all 'because' in the following sentence: 
+  'You cannot end a sentence with because because because is a conjunction' */
+  console.log('You cannot end a sentence with because because because is a conjunction'.match(/because/gi));
+
+  /* 3.- Use match() to count the number of all because in the following sentence:
+  'You cannot end a sentence with because because because is a conjunction' */
+  
