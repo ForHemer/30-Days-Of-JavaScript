1acd9c76ca80ab5797698e6b2b1ebee34759b7fc
ForHemer <foreste.hemerson@gmail.com>
2022-09-27 11:49:42 -0400

    Complete level 1

---
+++ 00_Solutions/day-03/level1.js
@@ -0,0 +1,9 @@
+// 7.- Use the Date Object to do the following activities
+const now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth()+1);
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getTime());
